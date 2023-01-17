6e6e1a3ee7586550940ddc562ceec60f2ad6dd24
BraIN <brainsvoboda@mail.ru>
2023-01-17 12:22:12 +0300

    task2 completed

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ЗАДАНИЕ 2
+int a;
+int b;
+int c;
+System.Console.Write("Введите число a: ");
+a=Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число b: ");
+b=Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число c: ");
+c=Convert.ToInt32(System.Console.ReadLine());
+
+if (a>b&&a>c){
+    System.Console.WriteLine(a.ToString()+","+b.ToString()+","+c.ToString()+" -> max = "+a.ToString());
+}else if(b>a&&b>c){
+    System.Console.WriteLine(a.ToString()+","+b.ToString()+","+c.ToString()+" -> max = "+b.ToString());
+}else if(c>a&&c>b){
+    System.Console.WriteLine(a.ToString()+","+b.ToString()+","+c.ToString()+" -> max = "+c.ToString());
+}else{
+    System.Console.WriteLine(a.ToString()+","+b.ToString()+","+c.ToString()+" -> max = Возможно есть 2 максимума");
+}
