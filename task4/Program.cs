b087c3394c7fbcdc28e6a1e119bda055b86d39cc
BraIN <brainsvoboda@mail.ru>
2023-01-17 15:19:35 +0300

    task4 complete

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+int n;
+System.Console.Write("Введите число n: ");
+n=Convert.ToInt32(System.Console.ReadLine());
+for (int i=1;i<=n;i++){
+    if (i%2==0){
+        System.Console.Write(i.ToString()+" ");
+    }
+}
