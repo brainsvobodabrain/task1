4f658dfdf6d794570e9d5603697e158fbfa3a679
BraIN <brainsvoboda@mail.ru>
2023-01-17 15:15:50 +0300

    task3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task 3
+int a;
+System.Console.Write("Введите число a: ");
+a=Convert.ToInt32(System.Console.ReadLine());
+if (a%2==0){
+    System.Console.WriteLine("Да");
+}else{
+    System.Console.WriteLine("Нет");
+}
