a592d0bfe8fdf64f57715768b50957fceed55cf1
BraIN <brainsvoboda@mail.ru>
2023-01-17 12:13:55 +0300

    New

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.gms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int a;
+int b;
+System.Console.Write("Введите число a: ");
+a=Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число b: ");
+b=Convert.ToInt32(System.Console.ReadLine());
+if (a>b){
+    System.Console.WriteLine(@"a={a}");
+}
--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//ЗАДАНИЕ 1
 Console.WriteLine("Hello, World!");
+int a;
+int b;
+System.Console.Write("Введите число a: ");
+a=Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число b: ");
+b=Convert.ToInt32(System.Console.ReadLine());
+if (a>b){
+    System.Console.WriteLine("a = "+a.ToString()+", b = "+b.ToString()+", max = "+a.ToString());
+}else{
+    System.Console.WriteLine("a = "+a.ToString()+", b = "+b.ToString()+", max = "+b.ToString());
+}
